9b264eb83675b0901fc71bfd2b2afb676e6da823
Harmmike <harmmike46@gmail.com>
2020-09-17 21:19:17 +0800

    Added error logging system
    Basic, first draft of an error logger that writes an xml node with a date and the error message body.

--- Assets/Scripts/Navigation/Navigator.cs
+++ Assets/Scripts/Navigation/Navigator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using UnitConverter.Logging.Logging;
 using UnityEngine;
 
 /// <summary>
---
+++ UnitConverter.Logging/Logging/UCErrorLogger.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace UnitConverter.Logging.Logging
+{
+    public static class UCErrorLogger
+    {
+
+        //TODO: Add email system to send error logs.
+
+        private const string ERROR_LOG_FILEPATH = ".\\Data\\ErrorLogs.xml";
+        public static void LogError(Exception exception)
+        {
+            DateTime eDate = DateTime.Now;
+            string errorBody = exception.Message;
+
+            if (File.Exists(ERROR_LOG_FILEPATH))
+            {
+                AddErrorRecord(eDate, errorBody);
+            }
+            else
+            {
+                CreateXmlLogFile(eDate, errorBody);
+            }
+        }
+
+        private static void AddErrorRecord(DateTime eDate, string errorBody)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(ERROR_LOG_FILEPATH);
+            XmlNode error = document.CreateElement("ERROR");
+            XmlNode date = document.CreateElement("DATE");
+            XmlNode body = document.CreateElement("BODY");
+
+            date.InnerText = eDate.ToShortDateString();
+            body.InnerText = errorBody;
+
+            error.AppendChild(date);
+            error.AppendChild(body);
+
+            document.DocumentElement.AppendChild(error);
+            document.Save(ERROR_LOG_FILEPATH);
+        }
+
+        private static void CreateXmlLogFile(DateTime eDate, string errorBody)
+        {
+            XmlDocument document = new XmlDocument();
+            XmlElement root = document.CreateElement("ERRORS");
+            document.AppendChild(root);
+            document.Save(ERROR_LOG_FILEPATH);
+
+            AddErrorRecord(eDate, errorBody);
+        }
+    }
+}
